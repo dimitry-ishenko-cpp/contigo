8f2f4db3320dc8a4ad57fb7c169fb28fc8d323ea
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2025-10-18 13:19:28 -0400

    bitmap: T => C

--- src/bitmap.hpp
+++ src/bitmap.hpp
@@ -11,18 +11,18 @@
 #include <memory>
 
 ////////////////////////////////////////////////////////////////////////////////
-template<typename T>
+template<typename C>
 class bitmap
 {
     struct dim dim_;
     unsigned stride_;
 
-    std::unique_ptr<T[]> data_;
+    std::unique_ptr<C[]> data_;
 
 public:
     ////////////////////
     bitmap(struct dim dim, unsigned stride) :
-        dim_{dim}, stride_{stride}, data_{std::make_unique<T[]>(dim_.height * stride_)}
+        dim_{dim}, stride_{stride}, data_{std::make_unique<C[]>(dim_.height * stride_)}
     { }
     explicit bitmap(struct dim dim) : bitmap{dim, dim.width} { }
 
@@ -35,5 +35,5 @@ public:
     constexpr auto data() noexcept { return data_.get(); }
 
     constexpr auto size() const noexcept { return dim_.height * stride_; }
-    constexpr auto size_bytes() const noexcept { return size() * sizeof(T); }
+    constexpr auto size_bytes() const noexcept { return size() * sizeof(C); }
 };
