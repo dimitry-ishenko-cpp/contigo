1116f04b4aafcfae0cd4ac9e2e6ca0481de500be
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2025-11-08 17:15:41 -0500

    Add bits for mouse handling

---
+++ src/mouse.cpp
@@ -0,0 +1,74 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2025 Dimitry Ishenko
+// Contact: dimitry (dot) ishenko (at) (gee) mail (dot) com
+//
+// Distributed under the GNU GPL license. See the LICENSE.md file for details.
+
+////////////////////////////////////////////////////////////////////////////////
+#include "error.hpp"
+#include "mouse.hpp"
+#include "vte.hpp"
+
+#include <algorithm> // std::clamp
+#include <asio/buffer.hpp>
+#include <asio/read.hpp>
+
+#include <fcntl.h> // open
+
+////////////////////////////////////////////////////////////////////////////////
+namespace mouse
+{
+
+namespace
+{
+
+auto open(const asio::any_io_executor& ex)
+{
+    auto fd = ::open("/dev/input/mice", O_RDONLY | O_NONBLOCK);
+    if (fd < 0) throw posix_error{"open"};
+
+    return asio::posix::stream_descriptor{ex, fd};
+}
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+device::device(const asio::any_io_executor& ex, unsigned rows, unsigned cols) :
+    fd_{open(ex)}
+{
+    resize(rows, cols);
+    sched_async_read();
+}
+
+void device::sched_async_read()
+{
+    asio::async_read(fd_, asio::buffer(&event_, sizeof(event_)), [&](std::error_code ec, std::size_t)
+    {
+        if (!ec)
+        {
+            row_ = std::clamp(row_ - event_.dy / 2, 0, max_row_);
+            col_ = std::clamp(col_ + event_.dx / 2, 0, max_col_);
+            if (move_cb_) move_cb_(row_, col_);
+
+            auto& state = event_.state;
+            if (button_cb_)
+            {
+                if (state.left  != state_.left ) { button_cb_(vte::button_left , state.left ); }
+                if (state.mid   != state_.mid  ) { button_cb_(vte::button_mid  , state.mid  ); }
+                if (state.right != state_.right) { button_cb_(vte::button_right, state.right); }
+            }
+            state_ = state;
+
+            sched_async_read();
+        }
+    });
+}
+
+void device::resize(unsigned rows, unsigned cols)
+{
+    max_row_ = rows - 1;
+    max_col_ = cols - 1;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+}
---
+++ src/mouse.hpp
@@ -0,0 +1,67 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2025 Dimitry Ishenko
+// Contact: dimitry (dot) ishenko (at) (gee) mail (dot) com
+//
+// Distributed under the GNU GPL license. See the LICENSE.md file for details.
+
+////////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include <asio/any_io_executor.hpp>
+#include <asio/posix/stream_descriptor.hpp>
+#include <cstdint>
+#include <functional>
+
+namespace vte { enum button : unsigned; }
+
+////////////////////////////////////////////////////////////////////////////////
+namespace mouse
+{
+
+////////////////////////////////////////////////////////////////////////////////
+class device
+{
+public:
+    ////////////////////
+    device(const asio::any_io_executor&, unsigned rows, unsigned cols);
+
+    using moved_callback = std::function<void(int row, int col)>;
+    void on_moved(moved_callback cb) { move_cb_ = std::move(cb); }
+
+    using button_changed_callback = std::function<void(vte::button, bool state)>;
+    void on_button_changed(button_changed_callback cb) { button_cb_ = std::move(cb); }
+
+    void resize(unsigned rows, unsigned cols);
+
+  private:
+    ////////////////////
+    asio::posix::stream_descriptor fd_;
+
+    moved_callback move_cb_;
+    button_changed_callback button_cb_;
+
+    int max_row_, max_col_;
+    int row_ = 0, col_ = 0;
+
+#pragma pack(push, 1)
+    struct button
+    {
+        bool left :1, right :1, mid :1;
+    }
+    state_;
+    static_assert(sizeof(button) == 1);
+
+    struct event
+    {
+        button state;
+        std::int8_t dx, dy;
+    }
+    event_;
+    static_assert(sizeof(event) == 3);
+#pragma pack(pop)
+
+    void sched_async_read();
+};
+
+////////////////////////////////////////////////////////////////////////////////
+}
